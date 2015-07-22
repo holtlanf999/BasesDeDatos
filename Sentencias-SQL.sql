c64a822719fd9bca743be5ed778752f25246bd6e
Lanford Gabriel Murillo <holtlanf999@hotmail.com>
2015-07-22 15:49:29 -0600

    agregado sentencias-SQL-s

---
+++ Sentencias-SQL.sql
@@ -0,0 +1,7 @@
+INSERT INTO cliente (nombre_cliente, ubicacion_cliente, estado_cliente) VALUES ('Parque La Libertad', 'Desamparados', 1);
+INSERT INTO cliente (nombre_cliente, ubicacion_cliente, estado_cliente) VALUES ('Mall San Pedro', 'Montes de Oca', 1);
+INSERT INTO cliente (nombre_cliente, ubicacion_cliente, estado_cliente) VALUES ('GREE', 'Estados Unidos', 0);
+
+INSERT INTO Proyecto ( id_cliente, tipo_proyecto, nombre_proyecto, precio_hora ) VALUES ( 1, 1, 'Rediseño sitio web', 25 );
+INSERT INTO Proyecto ( id_cliente, tipo_proyecto, nombre_proyecto, precio_hora ) VALUES ( 1, 2, 'Desarrollo sitio web', 30 );
+INSERT INTO Proyecto ( id_cliente, tipo_proyecto, nombre_proyecto, precio_hora ) VALUES ( 2, 1, 'HR Portal', 35 );
